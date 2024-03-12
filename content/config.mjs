cfc303bee321ce63e9b82b9e8e0d62e91bc77986
NuclaXx <nuclaxx@nuclaxx.fr>
2024-03-12 19:28:26 +0100

    Make Default Config

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,16 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+    title: "Portfolio NuclaXx",
+    description: "",
+    author: "NuclaXx",
+    domain: "https://nuclaxx.fr/",
+    favicon: "/images/logo.svg",
+    navbarTitle: {
+        text: "NuclaXx"
+      },
+    navLinks: [{ href: "/index", name: "Accueil" }],
+  };
+
+export default {config};
